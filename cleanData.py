a1467fc96548ebe330508e4a9e0b4dbf4c594ca4
root <root@Shreya-Laptop.>
2025-10-17 21:29:32 -0700

    Add small repo files (no large data or venv)

--- cleanData.py
+++ cleanData.py
@@ -1,6 +1,13 @@
 import pandas as pd 
+import json
 
-read_data = pd.read_parquet('project_b_samples_2k.parquet')
+def get_data():
+    read_data_omf = pd.read_parquet('project_b_samples_2k.parquet')
+    read_data_yelp = pd.read_json('yelp_academic_dataset_business.json', lines=True)
 
-print(read_data.info())
-print(read_data.head(100))
+
+def main():
+    get_data()
+
+if __name__ == "__main__":
+    main()
